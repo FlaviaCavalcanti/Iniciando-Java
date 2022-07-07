7828c29b822bbeee68b80a49b91eef1b679d735e
mayara <flavia.mayara2503@gmail.com>
2022-07-07 18:19:39 -0300

    Pequena mudança no exercicio

--- ExercíciosLaçoDecisão/Exercicio1.java
+++ ExercíciosLaçoDecisão/Exercicio1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int n1,n2,n3;
 		Scanner ler = new Scanner(System.in);
 		
